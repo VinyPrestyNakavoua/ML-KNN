a9179ea3ed7d49e5275ec97c0a9872ca52026ad5
Viny Presty NAKAVOUA <94742894+VinyPrestyNakavoua@users.noreply.github.com>
2024-11-22 15:28:46 +0100

    Add files via upload
    first commit knn

---
+++ knn.py
@@ -0,0 +1,31 @@
+## we have two types of machine learning : the supervised learning and the unsupervised one
+## supervised we give the input and the output when training the model
+## unsupervised we just give the input data
+
+### Supervised learning : Classification (categorical variable) and Regression (quantitative variable) 
+#### Classification : KNN algorithm
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+
+
+# import dataset
+iris_dataset = load_iris()
+
+# visualing data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
